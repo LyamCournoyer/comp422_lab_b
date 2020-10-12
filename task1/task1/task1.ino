b98209b53cdac5484ad52ac78c17a24bafaca3e7
LyamCournoyer <31738843+LyamCournoyer@users.noreply.github.com>
2020-10-12 16:20:06 -0400

    Done task1

---
+++ task1/task1/task1.ino
@@ -0,0 +1,19 @@
+byte INPUT_PIN = 6;
+byte OUTPUT_PIN = 13;
+void setup() { 
+  // put your setup code here, to run once:
+  pinMode(OUTPUT_PIN, OUTPUT);  
+  pinMode(INPUT_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+  if (digitalRead(INPUT_PIN)){
+    digitalWrite(OUTPUT_PIN, LOW);
+  }
+  else{
+    digitalWrite(OUTPUT_PIN, HIGH);
+  }
+}
