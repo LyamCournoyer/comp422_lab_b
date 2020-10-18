bb5239c65b26f92d42cf5025551dfee9dc8ac170
LyamCournoyer <31738843+LyamCournoyer@users.noreply.github.com>
2020-10-18 01:43:05 -0400

    Updated task3 with baremetal. Needs work

--- task3/task3.ino
+++ task3/task3.ino
@@ -1,25 +1,46 @@
-#define pin A0
+#include<avr/io.h>
+#define OutputLedDirection DDRB
+#define OutputLedRegister PORTB
+#define OutputBit (1 << 5)
+
+#define InputDirection DDRC
+#define InputPort PORTC
+#define InputPinRegister PINC
+#define InputBit (1 << 2)
+#define MinRange 670 //3.5 cm
+#define MaxRange 685 //4.5cm
+uint8_t low, high;
+uint8_t analog_reference = DEFAULT;
+
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin (9600);
-  pinMode (pin, INPUT);
+  OutputLedDirection |= OutputBit;
+  InputDirection  &= ~InputBit;
+
+  DIDR0 |= InputBit;
+  ADCSRA |= (1 << ADPS0) + (1 << ADPS1) + (1 << ADPS2);
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //enable ADC
+  ADMUX |= (1 << 6);
+  ADMUX |= (1 << MUX1); 
+  ADCSRA = ADCSRA | (1 << ADSC);
+  while(ADCSRA & (1 << ADSC));   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  uint16_t value = analogRead (pin);
-  uint16_t range = 4800/(analogRead(pin)-20);
+void loop() { 
+  ADCSRA = ADCSRA | (1 << ADSC);
+  while(ADCSRA & (1 << ADSC));   
+  low  = ADCL;
+  high = ADCH;
+   uint16_t value =(high << 8) | low; // ADCL & (1 << 2);
+   if ( value < MaxRange && value > MinRange){
+      OutputLedRegister |= OutputBit;
+   }
+   else{
+      OutputLedRegister &= ~OutputBit;
+   }
 
-//  get_gp2d12 (value);
-  Serial.println (value);
-  Serial.print (range);
-  Serial.println (" mm");
+  Serial.print (value);
   Serial.println ();
-  delay (1000);
-}
-
-uint16_t get_gp2d12 (uint16_t value) 
-{
-  if (value < 10) value = 10;
-  return ((67870.0 / (value - 3.0)) - 40.0);
 }
